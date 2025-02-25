4b47b5e760a50801636f1524d94da5b4b87f6cff
TamarGabay <G0556780416@GMAIL.COM>
2025-02-25 20:04:44 +0200

    uploud new file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,2 @@
-﻿namespace Program.cs
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
